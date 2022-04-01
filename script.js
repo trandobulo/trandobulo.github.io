178a881d2552f62972661427f299f64280fdb21c
Trandobulo Alexandr <trandobulo@gmail.com>
2022-04-01 21:56:39 +0300

    Added server request for sending email from Form
    + changes in structure of classes in css

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 "use strict"
 
-function print(){
+// function alertform(){
+//     event.preventDefault();
+//     console.log(event);
+//     let info=
+// `Ваши ФИО: ${feedbackform.name.value} 
+// Ваш телефон: ${feedbackform.phone.value} 
+// Ваше сообщение: ${feedbackform.msg.value}`;
 
-    let info=
-`Ваши ФИО: ${document.form.name.value} 
-Ваш телефон: ${document.form.phone.value} 
-Ваше сообщение: ${document.form.msg.value}`;
+// alert(info);
+// }
 
-    alert(info);
+function sendForm(){
+    event.preventDefault();
+    console.log(event);
+
+    const form = document.forms.feedbackform;
+
+    const user = { 
+        service_id: "service_it9lsv4",
+        user_id: "8-9J1lUVt13oYhM-q",
+        template_id: "feedbackform",
+        template_params: {
+            name: form.name.value,
+            phone: form.phone.value,
+            msg: form.msg.value
+        }
+    }
+
+let response = fetch ('https://api.emailjs.com/api/v1.0/email/send', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(user)
+    });
+    
+    alert("Ваши данные успешно отправлены")
 }
